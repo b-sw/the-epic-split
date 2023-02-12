1924fdb8bc11968d505c6b3c26e3f51ffaf0b78d
bartoszswitalski <bartoszswitalski@icloud.com>
2023-02-12 13:02:47 +0100

    WIP: quick commit

--- src/test/java/com/example/theepicsplit/listeners/FileTypeOpenedListenerTest.java
+++ src/test/java/com/example/theepicsplit/listeners/FileTypeOpenedListenerTest.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 import org.junit.Before;
-import org.junit.Test;
 
 class FileTypeOpenedListenerTest extends BasePlatformTestCase {
     private FileEditorManagerListener listener;
@@ -18,13 +17,5 @@ class FileTypeOpenedListenerTest extends BasePlatformTestCase {
         fileEditorManager = new FileEditorManagerImpl(getProject());
         listener = new FileTypeOpenedListener();
         fileEditorManager.addFileEditorManagerListener(listener);
-    }
-
-    @Test
-    void shouldOpenOnlyFileInOneTabGroup() {
-        // given no files are opened in intellij editor
-        // when a file is opened
-        // then the file is opened in one tab group
-
     }
 }
